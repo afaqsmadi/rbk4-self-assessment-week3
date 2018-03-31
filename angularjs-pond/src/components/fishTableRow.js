4a668a0f2b5f511f0b2628b089feb83411a85959
Mohammedalduraidi <Mohd.alduraidi@gmail.com>
2018-03-31 10:13:57 +0300

    attempeted

--- angularjs-pond/src/components/fishTableRow.js
+++ angularjs-pond/src/components/fishTableRow.js
@@ -3,6 +3,8 @@ angular.module('fish-pond')
   this.showDescription = false;
   this.toggleDescription = () => {
     this.showDescription = !this.showDescription;
+    this.fishes = fishData;
+    this.fish = this.fishes[0]
   };
 })
 .component('fishTableRow', {
@@ -11,11 +13,11 @@ angular.module('fish-pond')
   },
   controller: 'FishTableCtrl',
   template: `
-    <div ng-click="$ctrl.toggleDescription()">
-      <span class="fish-name">Nemo</span>
+    <div ng-click="$ctrl.toggleDescription()" ng-repeat="fish in $ctrl.fishes">
+      <span class="fish-name">{{fish.name}}</span>
       <span>
-        <img src="http://tinyurl.com/h8o5szh" />
+        <img src={{fish.image}} />
       </span>
-      <span class="fish-description" ng-if="$ctrl.showDescription">Does anyone know where my dad is?</span>
+      <span class="fish-description" ng-if="$ctrl.showDescription">{{fish.description}}</span>
     </div>`
 });
--- react-pond/src/components/FishTableRow.js
+++ react-pond/src/components/FishTableRow.js
@@ -10,11 +10,11 @@ class FishTableRow extends React.Component {
   render() {
     return (
       <tr onClick={() => this.setState({showDescription: !this.state.showDescription})}>
-        <td className="fish-name">Nemo</td>
+        <td className="fish-name">fishData.name</td>
         <td>
-          <img src="http://tinyurl.com/h8o5szh" />
+          <img src=fishData.image />
         </td>
-        {this.state.showDescription ? <td className="fish-description">Does anyone know where my dad is?</td> : null}
+        {this.state.showDescription ? <td className="fish-description">fishData.description</td> : null}
       </tr>
     )
   }
