ec9bc0d7d92d2276620d23a586520c39fc465d75
lenaSalamat <lenasalamat96@gmail.com>
2018-03-31 10:12:34 +0300

    lenasalamat

--- angularjs-pond/src/components/fishTableRow.js
+++ angularjs-pond/src/components/fishTableRow.js
@@ -12,10 +12,10 @@ angular.module('fish-pond')
   controller: 'FishTableCtrl',
   template: `
     <div ng-click="$ctrl.toggleDescription()">
-      <span class="fish-name">Nemo</span>
+      <span class="fishData.fish.name">{{Nemo}}</span>
       <span>
-        <img src="http://tinyurl.com/h8o5szh" />
+        <img src="fishData.fish.image" />
       </span>
-      <span class="fish-description" ng-if="$ctrl.showDescription">Does anyone know where my dad is?</span>
+      <span class="fishData.fish.description" ng-if="$ctrl.showDescription">Does anyone know where my dad is?</span>
     </div>`
 });
