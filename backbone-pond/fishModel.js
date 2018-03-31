67d1cbf63a9d43277bf0e9c95d540bf456839e09
Besslan <besslan.bazadough@gmail.com>
2018-03-31 10:10:06 +0300

    week4

--- backbone-pond/fishModel.js
+++ backbone-pond/fishModel.js
@@ -7,6 +7,18 @@ var Fish = Backbone.Model.extend({
     image: 'http://www.google.com',
     description: 'Regular old fish',
     displayInfo: false
+  },
+
+  toggleDescription: function () { 
+if (this.get("displayInfo")=== false) {
+this.set("displayInfo" , "true")
+} 
+else {
+	this.set("displayInfo" , "false")
+}
+
+
+
   }
 
 });
