8e0c891f50b030119993f14c7aa50d17d8de451d
Waed93 <alshyabwaed@gmail.com>
2018-03-31 08:54:58 -0400

    first

--- backbone-pond/fishModel.js
+++ backbone-pond/fishModel.js
@@ -7,6 +7,16 @@ var Fish = Backbone.Model.extend({
     image: 'http://www.google.com',
     description: 'Regular old fish',
     displayInfo: false
+  },
+  toggleDescription:function(){
+  	this.set('displayInfo',!this.get('displayInfo'))
   }
 
 });
+var instance=new Fish;
+this.on('click',{
+	instance.toggleDescription();
+})
+ // [ ] Inside of `fishModel.js`, create a `toggleDescription` function 
+ // such that when a fish <tr> is clicked, 
+ // the fish description will toggle on or off. DO NOT MODIFY any other files. 
